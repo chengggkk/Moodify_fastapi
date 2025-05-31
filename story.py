1d7c218986f22bd318e721d3180c5a4f25d81b74
chengggkk <chengggkk@gmail.com>
2025-05-31 12:05:46 +0800

    123

--- story.py
+++ story.py
@@ -48,6 +48,7 @@ Step 4: CREATIVE CATALYST IDENTIFICATION
 - Identify emotional triggers that create story momentum
 
 Please ONLY provide a RICH meaning STORY.
+FORMAT: HTML
                    """
     )
     if customize:
